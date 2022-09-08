7f19836292d6acdbe04245ed165754bada784998
Erdinc Akdogan <82614878+erdincakdogan@users.noreply.github.com>
2022-09-08 21:58:22 +0300

    Project folder created and loaders files added

---
+++ src/loaders/db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const db = mongoose.connection
+
+const connectDB = async () => {
+    db.once("open", () => {
+        console.log("Database connection succesfull")
+
+    })
+
+
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    }).then(() => {
+        console.log(`MongoDB connected  ${db.host}`)
+    }
+
+    ).catch(err => {
+        console.log(err)
+        process.exit(1)
+    })
+
+}
+
+module.exports = {
+    connectDB,
+}
