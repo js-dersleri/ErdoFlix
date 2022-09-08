ec9664e9dbde42bcf1ecf8c40ba9fef0c23629d4
Erdinc Akdogan <82614878+erdincakdogan@users.noreply.github.com>
2022-09-09 02:56:07 +0300

    Services files added

---
+++ src/services/BaseServices.js
@@ -0,0 +1,26 @@
+class BaseService {
+    constructor(model) {
+        this.BaseModel = model
+    }
+    list(where) {
+        return this.BaseModel.find(where || {})
+    }
+    insert(data) {
+        return new this.BaseModel(data).save()
+    }
+    findOne(where) {
+        return this.BaseModel.findOne(where)
+    }
+    changePassword(where, data) {
+        return this.BaseModel.findOneAndUpdate(where, data, { new: true })
+    }
+    update(id, data) {
+        return this.BaseModel.findByIdAndUpdate(id, data, { new: true })
+    }
+    delete(id) {
+        return this.BaseModel.findByIdAndDelete(id)
+    }
+
+}
+
+module.exports = BaseService
